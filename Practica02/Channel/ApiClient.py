4d879d39369f7c7f5565b3325c57b3c151eef745
marcobravociencias <marcobravo@ciencias.unam.mx>
2016-08-18 21:29:41 -0500

    practica02

--- Practica02/Channel/ApiClient.py
+++ Practica02/Channel/ApiClient.py
@@ -1,5 +1,45 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+from PyQt4 import QtCore, QtGui, uic
+from PyQt4.Qt import *
+import xmlrpclib
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import threading
+import time
+#from chatN import Chat
 
-class MyApiClient:
-    #TODO
+class MyApiClient():
+	def __init__(self, usr, ip1, port):
+		self.usr = usr
+		self.ip1 = ip1
+		self.port = port
+		#self.ip2 = ip2
+		#self.ApiServer = xmlrpclib.ServerProxy("http://"+self.ip2+":8000",allow_none=True) 
+		#self.tem = threading.Event()
+		#self.chat = Chat(self.tem)
+		#self.chat.show()
+		#self.chat.boton_send.clicked.connect(self.enviar)
+		#self.hiloClient = threading.Thread(target=self.escucha)
+		#self.hiloClient.start()
+
+	#def escucha():
+	#	while True:
+	#		time.sleep(1)
+	#		server = xmlrpclib.ServerProxy("http://"+self.ip1+":8000",allow_none=True) 
+	#		msj = server.vaciaBuffer()
+	#		if(len(msj)!=0):
+	#			#self.chat.setTexto(msj)
+	#			print 'MI mensaje es :'+msj
+		
+	def enviar():
+		print 'Mando mensaje'
+		if(self.ApiServer.ping()):
+			print 'Entramos'
+			msj = self.usr+' : '#+str(self.chat.text_send.toPlainText())+'\n'
+			self.ApiServer.sendMessage_wrapper(msj)
+			#self.chat.setTexto(msj)
+			print 'se hizo ping con exito'
+		
+		
--- Practica02/Channel/ApiServer.py
+++ Practica02/Channel/ApiServer.py
@@ -1,21 +1,35 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import sys
 
-
-class MyApiServer:
+class MyApiServer(SimpleXMLRPCRequestHandler):
     def __init__(self, my_port = None):
-        #TODO
+    	#TODO
+    	rpc_paths = ('/RPC2',)
+        
 class FunctionWrapper:
     def __init__(self):
         #TODO
+        self.buffer = list()
 
-     """ **************************************************
+    """ **************************************************
     Procedimiento que ofrece nuestro servidor, este metodo sera llamado
     por el cliente con el que estamos hablando, debe de
     hacer lo necesario para mostrar el texto en nuestra pantalla.
     ************************************************** """
     def sendMessage_wrapper(self, message):
         #TODO
+        self.buffer.append(message)
+    
+    def ping(self):
+        return True
 
-
-
+    def vaciaBuffer(self):
+        buffer = ''
+        for m in self.buffer:
+            buffer += m
+            print buffer
+            self.buffer = list()
+        return buffer
