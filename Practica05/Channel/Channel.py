c2b5012fc76489f38da5e702fba94e1b4fc52214
Anuar Tapia <anuartap@ciencias.unam.mx>
2016-09-24 15:14:18 -0500

    crea las clases RequestChannel y BidirectionalChannel en Channel

--- Practica05/Channel/Channel.py
+++ Practica05/Channel/Channel.py
@@ -4,32 +4,20 @@ from ApiServer import MyApiServer
 from ApiClient import MyApiClient
 
 
-class Channel:
+class RequestChannel():
 
-    """**************************************************
-    Las instancias de esta clase contendrán los métodos
-    necesarios para hacer uso de los métodos
-    del API de un contacto. Internamente trabajará
-    con una proxy apuntando hacia los servicios del
-    servidor xmlrpc del contacto
-    **************************************************"""
+    """
+    Contiene los métodos necesarios para hacer uso de los métodos de un
+    contacto (ApiClient)
+    """
 
-    def __init__(self, contact_ip=None, contact_port=None):
-        """**************************************************
-        Constructor de la clase
-        @param <str> contact_ip: Si no se trabaja de manera local representa la
-            ip del contacto con el que se establecera la conexión
-        @param <int> my_port: De trabajar de manera local puerto de la
-            instancia del cliente
-        @param <int> contact_port: De trabajar de manera local representa el
-            puerto de la instancia del contacto
-        **************************************************"""
-        self.contact_ip = contact_ip
-        self.contact_port = contact_port
+    def __init__(self, arg):
+        self.arg = arg
 
-    def send_text(self, text):
-        """**************************************************
-        Método que se encarga de mandar texto al contacto con
-        el cual se estableció la conexión
-        **************************************************"""
-        pass
+class BidirectionalChannel():
+	"""
+	Canala de comunicación bidireccional (ApiServer + ApiClient)
+	"""
+	def __init__(self, arg):
+		self.arg = arg
+		
