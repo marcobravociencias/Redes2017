5a168b642c0630e0e1ded3717cf64d2652e9abc8
Anuar Tapia <anuartap@ciencias.unam.mx>
2016-09-24 15:14:52 -0500

    Define el servidor de contactos GeneralDirectory

--- Practica05/Directory/Directory.py
+++ Practica05/Directory/Directory.py
@@ -7,16 +7,23 @@ from Mensaje import *
 import sys
 import getopt
 
+
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
+
+# class ServidorContactos:
 class GeneralDirectory:
+
     """
     Servidor que mantiene una lista de usuarios conectados en la red
     """
+
     def __init__(self):
-        #lista de tuplas (alias, ip)
-        self.contactos = list()
+        # lista de tuplas (alias, ip)
+        # self.contactos = list()
+        # lista de tuplas (alias, ip, puerto)
+        self.users = list()
 
     def ping(self):
         """
@@ -24,27 +31,31 @@ class GeneralDirectory:
         """
         return True
 
-    def login(self,contacto):
+    # def login(self, contacto):
+    def connect_wrapper(self, username):
         """
         Agrega un contacto a la lista de contactos disponibles
         """
-        if contacto in self.contactos:
+        if username in self.users:
             return None
         else:
-            self.contactos.append(contacto)
-            return self.contactos
+            self.users.append(username)
+            return self.users
 
-    def logout(self,contacto):
+    # def logout(self, contacto):
+    def disconnect_wrapper(self, username):
         """
         Quita un contacto a la lista de contactos disponibles
         """
-        self.contactos.remove(contacto)
+        self.users.remove(username)
 
-    def disponibles(self):
+    # def disponibles(self):
+    def get_contacts_wrapper(self, username):
         """
         Regresa la lista de contactos conectados
         """
-        return self.contactos
+        return self.users
+
 
 def main():
     # parseo de los argumentos de la línea de comandos
@@ -53,9 +64,9 @@ def main():
     except getopt.error, msg:
         print msg
         sys.exit(2)
-    ip = args[0] # obtenemos la ip del servidor de contactos con el primer argumento
+    ip = args[0]  # obtenemos la ip del servidor de contactos con el primer argumento
     # iniciamos el servidor de contactos con la ip obtenida en el puerto 8001
-    server = SimpleXMLRPCServer((ip, 8001),requestHandler=RequestHandler,allow_none=True)
+    server = SimpleXMLRPCServer((ip, 8001), requestHandler=RequestHandler, allow_none=True)
     server.register_introspection_functions()
     server.register_instance(GeneralDirectory())
     try:
