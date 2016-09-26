ea16c166ac8ce7faeb030a61f794413ddfb66a77
Anuar Tapia <anuartap@ciencias.unam.mx>
2016-09-25 19:57:53 -0500

    crea función send_text en ApiClient, inicializa servidor de ApiClient, agrega clase Message

--- Practica05/Channel/ApiClient.py
+++ Practica05/Channel/ApiClient.py
@@ -14,11 +14,28 @@ los procedimientos remotos que ofrece la api del contacto
 
 class MyApiClient:
 
-    def __init__(self, contact_ip=None, contact_port=None):
-        if contact_port:
-            contact_ip = get_ip_address()
-            self.server = xmlrpclib.ServerProxy("http://" + contact_ip + ":" + contact_port, allow_none= True)
-        elif contact_ip:
-            self.server = xmlrpclib.ServerProxy("http://"+self.ip+":"+Constants.PORT, allow_none=True)
-        else:
-            raise ValueError('The values of fields are not consistent MyApiClient.__init__')
+    def __init__(self, username, contact_ip=None, contact_port=None):
+        self.username_local = username
+        self.ip_local = get_ip_address()
+        try:
+            if contact_port:
+                # ip local + puerto indicado
+                self.ip_remote = self.ip_local
+                self.server = xmlrpclib.ServerProxy("http://" + self.ip_local + ":" + contact_port, allow_none=True)
+            elif contact_ip:
+                # ip indicado + PORT
+                self.ip_remote = contact_ip
+                self.server = xmlrpclib.ServerProxy("http://"+contact_ip+":"+Constants.PORT, allow_none=True)
+            else:
+                raise ValueError('The values of fields are not consistent MyApiClient.__init__')
+            self.username_remote = username  # TODO
+        except Exception, e:
+            raise e
+
+    def send_text(self, message):
+        wrapped_message = Message(self.username, self.ip_local, message)
+        try:
+            self.server.sendMessage_wrapper(wrapped_message)
+            # TODO dibuja el mensaje en la ventana de chat correspondiente
+        except Exception, e:
+            raise e
---
+++ Practica05/Channel/Message.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import time
+
+class Message():
+    """Clase que define el formato para un mensaje"""
+    def __init__(self, author_username, author_ip, content):
+        self.date = time.strftime("%H:%M:%S")
+        self.author_username = author_username
+        self.author_ip = author_ip
+        self.content = content
+        self.read = False
+
+    def setRead(self, read):
+    	self.read = read
+        
+    def getRead(self):
+    	return self.read
