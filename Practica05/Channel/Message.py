6b6b2c157d11e2947fe991b56a3b524c723678d1
Anuar Tapia <anuartap@ciencias.unam.mx>
2016-09-25 20:28:07 -0500

    agrega date como param del constructor de Message

--- Practica05/Channel/Message.py
+++ Practica05/Channel/Message.py
@@ -8,8 +8,8 @@ class Message():
 
     """Clase que define el formato para un mensaje"""
 
-    def __init__(self, author_username, author_ip, content):
-        self.date = time.strftime("%H:%M:%S")
+    def __init__(self, author_username, author_ip, content, date = time.strftime("%H:%M:%S")):
+        self.date = date
         self.author_username = author_username
         self.author_ip = author_ip
         self.content = content
